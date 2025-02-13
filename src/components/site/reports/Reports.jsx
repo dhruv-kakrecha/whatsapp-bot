d4d48ef2da44c80a13fd366ca11c89a4b26ab3b0
dhruv-kakrecha <160730440+dhruv-kakrecha@users.noreply.github.com>
2025-02-13 17:49:02 +0530

    changes

--- src/components/site/reports/Reports.jsx
+++ src/components/site/reports/Reports.jsx
@@ -1,66 +1,88 @@
-import { CheckOutlined, EyeOutlined, SendOutlined } from '@ant-design/icons'
-import { PageContainer, ProCard } from '@ant-design/pro-components'
-import { Card, Col, Flex, Row, Typography } from 'antd'
-import React from 'react'
-import { FaReplyAll } from 'react-icons/fa'
-import { HiMiniSquare3Stack3D, HiOutlineArrowPathRoundedSquare } from 'react-icons/hi2'
-import { IoCheckmarkDone } from 'react-icons/io5'
-import { RxCrossCircled } from 'react-icons/rx'
+import React, { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import { CheckOutlined, EyeOutlined, QuestionCircleOutlined, SendOutlined } from '@ant-design/icons';
+import { PageContainer, ProCard } from '@ant-design/pro-components';
+import { Card, Col, DatePicker, Flex, Row, Tooltip, Typography } from 'antd';
+import { FaReplyAll } from 'react-icons/fa';
+import { HiMiniSquare3Stack3D, HiOutlineArrowPathRoundedSquare } from 'react-icons/hi2';
+import { IoCheckmarkDone } from 'react-icons/io5';
+import { RxCrossCircled } from 'react-icons/rx';
+import moment from 'moment';
+import axiosInstance from '../../../axios/axiosInstance';
+import { useSelector } from 'react-redux';
 
 const Reports = () => {
+    const { RangePicker } = DatePicker;
+    const { user } = useSelector(state => state.auth)
+    console.log("user", user);
 
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
 
-    const data = [
-        {
-            title: "Sent",
-            value: 0,
-            icon: <CheckOutlined />
-        },
-        {
-            title: "Deliverd",
-            value: 0,
-            icon: <IoCheckmarkDone />
-        },
-        {
-            title: "Read",
-            value: 0,
-            icon: <EyeOutlined />
-        },
-        {
-            title: "Replied",
-            value: 0,
-            icon: <FaReplyAll />
+    const getStatisticsData = async (dateRange) => {
+        setLoading(true)
+        try {
+            const payload = {
+                username: "Drdiamondauto@gmail.com",
+                dateFrom: dateRange[0].set('hour', 0).set('minute', 0).set('second', 0).toISOString(),
+                dateTo: dateRange[1].set('hour', 23).set('minute', 59).set('second', 59).toISOString(),
+                searchString: "",
+            };
+            const { data } = await axiosInstance.post("/statistics/get", payload);
+            if (data?.success) {
+                setData([
+                    { title: "Sent", value: data?.data?.totalSent, icon: <CheckOutlined /> },
+                    { title: "Delivered", value: data?.data?.totalDelivered, icon: <IoCheckmarkDone /> },
+                    { title: "Read", value: data?.data?.totalSent, icon: <EyeOutlined /> },
+                    { title: "Replied", value: data?.data?.totalReplied, icon: <FaReplyAll /> },
+                    { title: "Sending", value: data?.data?.totalSending, icon: <SendOutlined /> },
+                    { title: "Failed", value: data?.data?.totalFailed, icon: <RxCrossCircled /> },
+                    { title: "Processing", value: data?.data?.totalProcessing, icon: <HiOutlineArrowPathRoundedSquare /> },
+                    { title: "Queued", value: data?.data?.totalQueued, icon: <HiMiniSquare3Stack3D /> },
+                ]);
+                setLoading(false);
+            }
+            console.log("Statistics Data: ", data);
+        } catch (error) {
+            console.error("Error fetching statistics:", error);
+        }
+    };
 
-        },
-        {
-            title: "Sending",
-            value: 0,
-            icon: <SendOutlined />
-        },
-        {
-            title: "Failed",
-            value: 0,
-            icon: <RxCrossCircled />
-        },
-        {
-            title: "Processing",
-            value: 0,
-            icon: <HiOutlineArrowPathRoundedSquare />
-        },
-        {
-            title: "Queued",
-            value: 0,
-            icon: <HiMiniSquare3Stack3D />
-        },
-    ]
+    const handleDateChange = async (dates) => {
+        console.log("_dates", dates);
+
+        if (dates) {
+            const formattedDates = [dates[0].toISOString(), dates[1].toISOString()];
+            console.log("Selected Dates: ", formattedDates);
+            await getStatisticsData(dates);
+        }
+    };
+
+    useEffect(() => {
+        const initialDateFrom = dayjs('2025/01/01', 'YYYY/MM/DD').set('hour', 0).set('minute', 0).set('second', 0);
+        const initialDateTo = dayjs(new Date()).set('hour', 23).set('minute', 59).set('second', 59);
+        getStatisticsData([initialDateFrom, initialDateTo]);
+    }, []);
     return (
         <PageContainer
             title="Reports"
         >
-            <ProCard title="Statistics">
+            <ProCard loading={loading} title="Statistics">
+                <Card style={{ marginBlock: 24 }}>
+                    <Flex justify='end'>
+                        <RangePicker
+                            defaultValue={[
+                                dayjs('2025/01/01', 'YYYY/MM/DD').set('hour', 0).set('minute', 0).set('second', 0),
+                                dayjs(new Date()).set('hour', 23).set('minute', 59).set('second', 59)
+                            ]}
+                            format="YYYY/MM/DD"
+                            onChange={handleDateChange}
+                        />
+                    </Flex>
+                </Card>
                 <Row gutter={[16, 24]}>
                     {data?.map((item, index) => (
-                        <Col key={index} span={3}>
+                        <Col key={index} xs={12} sm={6} md={6} lg={6} xl={3} >
                             <Card bodyStyle={{ padding: 15 }} hoverable>
                                 <Flex justify='space-evenly' align='center'>
                                     <div>
@@ -68,7 +90,7 @@ const Reports = () => {
                                             {item.value}
                                         </Typography.Title>
                                         <Typography.Title level={5} style={{ padding: 0, margin: 0 }}>
-                                            {item?.title}
+                                            {item?.title} <Tooltip title={item?.title}><QuestionCircleOutlined style={{ fontSize: 12, opacity: 0.5 }} /></Tooltip>
                                         </Typography.Title>
                                     </div>
                                     <div style={{ fontSize: 20 }}>
